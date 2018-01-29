29eee603b7c97be49cbc01485cfce28c9494b7c6
Alexander Klyukin <a.g.klyukin@gmail.com>
2018-01-29 23:06:04 +0300

    бред Мэин и Регистрэйшн

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         MyForm app = new MyForm();
         app.setVisible(true);
-        // хуй
     }
 }
