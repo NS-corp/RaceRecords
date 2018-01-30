30f250fe3b9dac433e4c6ceaf1882acdcc648575
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-01-30 20:45:30 +0300

    Добавил сохранение в PDF

--- src/com/company/PdfSaver.java
+++ src/com/company/PdfSaver.java
@@ -5,59 +5,48 @@ import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Vector;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.PageSize;
 import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class PdfSaver {
-    final static String FONT_PATH = "\\C:\\Универ\\ООП\\Лабы\\Лабы 2-10\\resources\\Шрифты\\arial.ttf";
-    public static void savePdfFile(DefaultTableModel tableModel){
+    final static String FONT_PATH = MyForm.resourcesPath + "\\Шрифты\\arial.ttf";
+    public static void savePdfFile(TableModel tableModel, String filePath){
         Document document = new Document(PageSize.A4, 50, 50 ,50 ,50);
 
-        PdfPTable t= new PdfPTable(4);
-
-        try {
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("\\C:\\Универ\\ООП\\Лабы\\Лабы 2-10\\resources\\RaceInfo.pdf"));
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        } catch (DocumentException e){
-            e.printStackTrace();
-        }
+        PdfPTable pdfTable = new PdfPTable(tableModel.getColumnCount());
 
         BaseFont bfComic = null;
         try {
-            bfComic = BaseFont.createFont("\\C:\\Универ\\ООП\\Лабы\\Лабы 2-10\\resources\\Шрифты\\arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            bfComic = BaseFont.createFont(FONT_PATH, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
         } catch (DocumentException el){
             el.printStackTrace();
         } catch (IOException el){
             el.printStackTrace();
         }
 
-        Font font1= new Font(bfComic, 12);
+        Font font1 = new Font(bfComic, 12);
 
-        t.addCell(new PdfPCell(new Phrase("Гонщик",font1)));
-        t.addCell(new PdfPCell(new Phrase("Команда",font1)));
-        t.addCell(new PdfPCell(new Phrase("Призёр на трассе",font1)));
-        t.addCell(new PdfPCell(new Phrase("Количество очков",font1)));
+        // Добавляем заголовки
+        tableModel.getHeaders().forEach(header -> {
+            pdfTable.addCell(new PdfPCell(new Phrase((String) header, font1)));
+        });
 
-        for (int i=0; i< tableModel.getRowCount(); i++){
-            t.addCell(new Phrase((String) tableModel.getValueAt(i,0), font1));
-            t.addCell(new Phrase((String) tableModel.getValueAt(i,1), font1));
-            t.addCell(new Phrase((String) tableModel.getValueAt(i,2), font1));
-            t.addCell(new Phrase((String) tableModel.getValueAt(i,3), font1));
+        for (int rowInd = 0; rowInd < tableModel.getRowCount(); rowInd++){
+            for(int columnInd = 0; columnInd < tableModel.getColumnCount(); columnInd++){
+                pdfTable.addCell(new Phrase((String) tableModel.getValueAt(rowInd,columnInd), font1));
+            }
         }
 
         document.open();
 
         try {
-            document.add(t);
+            document.add(pdfTable);
         } catch (DocumentException e){
             e.printStackTrace();
         }
